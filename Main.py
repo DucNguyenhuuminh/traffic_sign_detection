a5490013e8ffa40b9f131d6b5700e491214e6a8f
unknown <https://github.com/DucNguyenhuuminh/Image-processing.git>
2025-05-12 20:40:47 +0700

    final

--- Main.py
+++ Main.py
@@ -10,89 +10,64 @@ import pandas as pd
 import cv2
 from sklearn.model_selection import train_test_split
 import os
-
+import random
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import pickle
+from tensorflow.keras.models import load_model
 
 # Setup data
-path = "dataset\myData"
-label_file = 'labels.csv'
-batch_size_val = 50
-steps_per_epoch_val = 2000
-epochs_val =10
-imageDim = (32,32,3)
-testRatio = 0.2         #if 1000 images split will 200 for testing
-validateRatio = 0.2     #if 1000 images 20% of remaining 800 will be 160 for validation
+path = "dataset/myData"  
+label_file = 'labels.csv'  
+batch_size_val = 64
+steps_per_epoch_val = 260
+epochs_val = 10
+imageDim = (32,32,3)  
+testRatio = 0.25         
+validateRatio = 0.25     
 
-#importing of the images
+# Importing images
 count = 0
 images = []
 classNo = []
 myList = os.listdir(path)
-print("Total classes detected:",len(myList))
+print("Total classes detected:", len(myList))
 numberClasses = len(myList)
 print("Importing Classes.......")
-for x in range(0,len(myList)):
+for x in range(0, len(myList)):
     imgList = os.listdir(f"{path}/{count}")
     for y in imgList:
         current = cv2.imread(f"{path}/{count}/{y}")
         images.append(current)
         classNo.append(count)
     print(count, end=" ")
-    count+=1
+    count += 1
 print(" ")
 images = np.array(images)
 classNo = np.array(classNo)
 
-#spliting data
+# Splitting data
 X_train, X_test, y_train, y_test = train_test_split(images, classNo, test_size=testRatio)
-X_train, X_validate, y_train, y_validate = train_test_split(X_train, y_train,test_size=validateRatio)
+X_train, X_validate, y_train, y_validate = train_test_split(X_train, y_train, test_size=validateRatio)
 
-#X_train = array of images to train
-#y_train = corresponding class id
-
-#check numbers of images matches to numbers lables 
+# Check shapes of data
 print("Data Shapes")
-print("Train", end="");print(X_train.shape, y_train.shape)
-print("Validation",end="");print(X_validate.shape, y_validate.shape)
-print("Test",end="");print(X_test.shape,y_test.shape)
-assert(X_train.shape[0]==y_train.shape[0]),"The number of images is not equals to the number of labels in training set"
-assert(X_validate.shape[0]==y_validate.shape[0]),"The number of images is not equals to the number of labels in validation set"
-assert(X_test.shape[0]==y_test.shape[0]),"The number of images is not equals to the number of lables in test set"
-assert(X_train.shape[1:]==(imageDim)),"The dimentions of the Training images are wrong"
-assert(X_validate.shape[1:]==(imageDim)),"The dimentions of the Validation images are wrong"
-assert(X_test.shape[1:]==(imageDim)),"The dimentiosn of the Test images are wrong"
+print("Train", X_train.shape, y_train.shape)
+print("Validation", X_validate.shape, y_validate.shape)
+print("Test", X_test.shape, y_test.shape)
 
-#read csv file
+assert(X_train.shape[0] == y_train.shape[0]), "The number of images is not equals to the number of labels in training set"
+assert(X_validate.shape[0] == y_validate.shape[0]), "The number of images is not equals to the number of labels in validation set"
+assert(X_test.shape[0] == y_test.shape[0]), "The number of images is not equals to the number of labels in test set"
+assert(X_train.shape[1:] == (imageDim)), "The dimensions of the Training images are wrong"
+assert(X_validate.shape[1:] == (imageDim)), "The dimensions of the Validation images are wrong"
+assert(X_test.shape[1:] == (imageDim)), "The dimensions of the Test images are wrong"
+
+# Read csv file
 data = pd.read_csv(label_file)
-print("data shape",data.shape,type(data))
+print("data shape", data.shape, type(data))
 
-#display samples images of all classes
-num_of_samples = []
-cols = 5
-num_class = numberClasses
-fig, axs = plt.subplots(nrows=num_class, ncols=cols, figsize=(5,300))
-fig.tight_layout()
-for i in range(cols):
-    for j,row in data.iterrows():
-        x_select = X_train[y_train == j]
-        axs[j][i].imshow(x_select[random.randint(0,len(x_select)-1),:,:],cmap=plt.get_cmap("gray"))
-        #axs[j][i].axis("off")
-        if i == 2:
-            axs[j][i].set_title(str(j) + " - " + row["Name"])
-            num_of_samples.append(len(x_select))
-
-#display a bar chart showing number of sample for each category
-plt.figure(figsize=(12,4))
-plt.bar(range(0, num_class),num_of_samples)
-plt.title("Distribution of the training dataset")
-plt.xlabel("Class number")
-plt.ylabel("Number of images")
-plt.show()
-
-#preprocessing the images
+# Preprocessing the images
 def grayscale(img):
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return img
 
 def equalize(img):
@@ -100,22 +75,21 @@ def equalize(img):
     return img
 
 def preprocessing(img):
-    img = grayscale(img)        #convert to grayscale
-    img = equalize(img)         #standardize the lighting in an image
-    img = img/255               #to nomalize values between 0 and 1 instead of 0 to 255
+    img = grayscale(img)       # Convert to grayscale
+    img = equalize(img)        # Standardize lighting in image
+    img = img / 255.0          # Normalize to values between 0 and 1
     return img
 
-X_train = np.array(list(map(preprocessing,X_train)))
-X_validate = np.array(list(map(preprocessing,X_validate)))
-X_test = np.array(list(map(preprocessing,X_test)))
-#cv2.imshow("GrayScale Images", X_train[random.randint(0,len(X_train)-1)])
+X_train = np.array(list(map(preprocessing, X_train)))
+X_validate = np.array(list(map(preprocessing, X_validate)))
+X_test = np.array(list(map(preprocessing, X_test)))
 
-# add a depth of 1
+# Reshaping images for input to CNN
 X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2], 1)
-X_validate = X_validate.reshape(X_validate.shape[0],X_validate.shape[1], X_validate.shape[2],1)
-X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[2],1)
+X_validate = X_validate.reshape(X_validate.shape[0], X_validate.shape[1], X_validate.shape[2], 1)
+X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[2], 1)
 
-#augmentation of images: to makeit more generic
+# Data augmentation
 dataGen = ImageDataGenerator(width_shift_range=0.1,
                              height_shift_range=0.1,
                              zoom_range=0.2,
@@ -123,79 +97,82 @@ dataGen = ImageDataGenerator(width_shift_range=0.1,
                              rotation_range=10)
 
 dataGen.fit(X_train)
-batches=dataGen.flow(X_train,y_train,batch_size=20)
-X_batch,y_batch = next(batches)
+batches = dataGen.flow(X_train, y_train, batch_size=20)
+X_batch, y_batch = next(batches)
 
-'''#show agmented image samples
-fig,axs = plt.subplots(1,15,figsize=(20,5))
-fig.tight_layout()
+# One-hot encoding of labels
+y_train = to_categorical(y_train, numberClasses)
+y_validate = to_categorical(y_validate, numberClasses)
+y_test = to_categorical(y_test, numberClasses)
 
-for i in range(15):
-    axs[i].imshow(X_batch[i].reshape(imageDim[0],imageDim[1]))
-    axs[i].axis('off')
-plt.show()'''
-
-y_train = to_categorical(y_train,numberClasses)
-y_validate = to_categorical(y_validate,numberClasses)
-y_test = to_categorical(y_test,numberClasses)
-
-#convolution neural network model
+# Convolutional Neural Network model
 def myModel():
     no_of_Filters = 60
-    size_of_Filter = (5,5)
-
-    size_of_Filter2 = (3,3)
-    size_of_pool = (2,2)
+    size_of_Filter = (5, 5)
+    size_of_Filter2 = (3, 3)
+    size_of_pool = (2, 2)
     no_of_Nodes = 500
+    
     model = Sequential()
-    model.add((Conv2D(no_of_Filters,size_of_Filter,input_shape=(imageDim[0],imageDim[1],1),activation='relu')))
-    model.add((Conv2D(no_of_Filters,size_of_Filter,activation='relu')))
+    model.add(Conv2D(no_of_Filters, size_of_Filter, input_shape=(imageDim[0], imageDim[1], 1), activation='relu'))
+    model.add(Conv2D(no_of_Filters, size_of_Filter, activation='relu'))
     model.add(MaxPooling2D(pool_size=size_of_pool))
-
-    model.add((Conv2D(no_of_Filters//2,size_of_Filter2,activation='relu')))
-    model.add((Conv2D(no_of_Filters//2,size_of_Filter2,activation='relu')))
+    
+    model.add(Conv2D(no_of_Filters//2, size_of_Filter2, activation='relu'))
+    model.add(Conv2D(no_of_Filters//2, size_of_Filter2, activation='relu'))
     model.add(MaxPooling2D(pool_size=size_of_pool))
-    model.add(Dropout(0.5))
-
+    model.add(Dropout(0.5))   
+    
     model.add(Flatten())
-    model.add(Dense(no_of_Nodes,activation='relu'))
+    model.add(Dense(no_of_Nodes, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(numberClasses,activation='softmax'))
-    #compile model
+    model.add(Dense(numberClasses, activation='softmax'))
+    
     model.compile(optimizer=Adam(learning_rate=0.001),
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
     return model
 
-#training
-model = myModel()
+# Training the model
+if os.path.exists("model_trained.keras"):
+    print("Loading saved model...")
+    model = load_model("model_trained.keras")
+else:
+    print("No saved model found, training new model...")
+    model = myModel()  
 print(model.summary())
-history = model.fit(dataGen.flow(X_train,y_train,batch_size=batch_size_val),
+history = model.fit(dataGen.flow(X_train, y_train, batch_size=batch_size_val),
                     steps_per_epoch=steps_per_epoch_val,
                     epochs=epochs_val,
-                    validation_data=(X_validate,y_validate),
+                    validation_data=(X_validate, y_validate),
                     shuffle=True)
 
-#plot
+# Plot training history
 plt.figure(1)
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
-plt.legend(['training','validation'])
-plt.title('loss')
-plt.xlabel('epoch')
+plt.legend(['training', 'validation'])
+plt.title('Loss')
+plt.xlabel('Epoch')
+
 plt.figure(2)
 plt.plot(history.history['accuracy'])
 plt.plot(history.history['val_accuracy'])
-plt.legend(['training','validation'])
-plt.title('accuracy')
-plt.xlabel('epoch')
+plt.legend(['training', 'validation'])
+plt.title('Accuracy')
+plt.xlabel('Epoch')
 plt.show()
-score = model.evaluate(X_test,y_test,verbose=0)
-print('Test Score:',score[0])
-print('Test Accuracy:',score[1])
 
-#store model as pickle
-pickle_out= open("model_trained.p","wb")
-pickle.dump(model,pickle_out)
-pickle_out.close()
-cv2.waitKey(0)
+# Evaluate the model on test data
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test Score:', score[0])
+print('Test Accuracy:', score[1])
+
+# Save model in .keras format
+model.save("model_trained.keras")  
+
+# Verify if model has been saved successfully
+if os.path.exists("model_trained.keras"):
+    print("Model saved successfully!")
+else:
+    print("Model save failed.")
--- TEST_model.py
+++ TEST_model.py
@@ -1,92 +1,95 @@
 import cv2
 import pickle
 import numpy as np
-import os
 
-#parameter for cam
+# Camera config
 frameWidth = 640
 frameHeight = 480
 brightness = 180
-threshold = 0.75
+threshold = 0.5  
 font = cv2.FONT_HERSHEY_SIMPLEX
+from tensorflow.keras.models import load_model
 
-#setup camera
+
+
+model = load_model("model_trained.keras")
+
+# Setup camera
 cap = cv2.VideoCapture(0)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10, brightness)
 
-#import the trained model
-pickle_in = open("model_trained.p","rb")
-model = pickle.load(pickle_in)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
 
+# Preprocessing
 def grayScale(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    return img
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 def equalize(img):
-    img = cv2.equalizeHist(img)
-    return img
+    return cv2.equalizeHist(img)
 
 def preprocessing(img):
     img = grayScale(img)
     img = equalize(img)
-    img = img / 255
+    img = cv2.GaussianBlur(img, (3,3), 0)
+    img = img / 255.0
     return img
 
-def getClassName(numberClasses):
-    if numberClasses == 0:  return 'No parking'
-    elif numberClasses == 1:    return 'Go over the roundabout'
-    elif numberClasses == 2:    return 'Right-of-way at the next intersection'
-    elif numberClasses == 3:    return 'Priority road'
-    elif numberClasses == 4:    return 'Yield'
-    elif numberClasses == 5:    return 'Stop'
-    elif numberClasses == 6:    return 'No vehicles'
-    elif numberClasses == 7:    return 'No entry'
-    elif numberClasses == 8:    return 'Dangerous curve to left'
-    elif numberClasses == 9:    return 'Dangerous curve to right'
-    elif numberClasses == 10:    return 'Double curve'
-    elif numberClasses == 11:    return 'Bumby road'
-    elif numberClasses == 12:    return 'Slippery road'
-    elif numberClasses == 13:    return 'Road narrowed on the right'
-    elif numberClasses == 14:    return 'Road work'
-    elif numberClasses == 15:    return 'Pedestrians'
-    elif numberClasses == 16:    return 'Childen crossing'
-    elif numberClasses == 17:    return 'Bicycles crossing'
-    elif numberClasses == 18:    return 'Turn right ahead'
-    elif numberClasses == 19:    return 'Turn left ahead'
-    elif numberClasses == 20:    return 'Ahead only'
-    elif numberClasses == 21:    return 'Go straight or right'
-    elif numberClasses == 22:    return 'Go straight or left'
-    elif numberClasses == 23:    return 'Go this way on the right'
-    elif numberClasses == 24:    return 'Go this way on the left'
-    elif numberClasses == 25:    return 'No left turn'
-    elif numberClasses == 26:    return 'No right turn'
-    elif numberClasses == 27:    return 'No automobiles'
-    elif numberClasses == 28:    return 'No stopping'
-    else:    return 'Unknown'
 
-while True:
-    #read image
-    success, imgOrigin = cap.read()
+# Class names
+def getClassName(classIndex):
+    classes = [
+        'No parking', 'Go over the roundabout', 'Right-of-way at the next intersection',
+        'Priority road', 'Yield', 'Stop', 'No vehicles', 'No entry',
+        'Dangerous curve to left', 'Dangerous curve to right', 'Double curve',
+        'Bumpy road', 'Slippery road', 'Road narrowed on the right', 'Road work',
+        'Pedestrians', 'Children crossing', 'Bicycles crossing', 'Turn right ahead',
+        'Turn left ahead', 'Ahead only', 'Go straight or right', 'Go straight or left',
+        'Go this way on the right', 'Go this way on the left', 'No left turn',
+        'No right turn', 'No automobiles', 'No stopping'
+    ]
+    return classes[classIndex] if 0 <= classIndex < len(classes) else 'Unknown'
 
-    #proccess image
-    img = np.asarray(imgOrigin)
-    img = cv2.resize(img,(32,32))
-    img = preprocessing(img)
-    cv2.imshow("Proccess Image", img)
-    img = img.reshape(1,32,32,1)
-    cv2.putText(imgOrigin, "CLASS: ", (20,35), font, 0.75, (0,0,255), 2, cv2.LINE_AA)
-    cv2.putText(imgOrigin,"PROBABILITY: ",(20,75), font, 0.75, (0,0,255),2,cv2.LINE_AA)
-
-    #Predict image
-    predicted = model.predict(img)
-    classIndex = model.predict_classes(img)
-    probabilityVal = np.amax(predicted)
-    if probabilityVal > threshold:
-        cv2.putText(imgOrigin,str(classIndex)+" "+str(getClassName(classIndex)),(120,35),font,0.75,(0,0,255),2,cv2.LINE_AA)
-        cv2.putText(imgOrigin,str(round(probabilityVal*100,2))+"%",(180,75),font,0.75,(0,0,255),2,cv2.LINE_AA)
-        cv2.imshow("Result", imgOrigin)
-
-        if cv2.waitKey(1) and 0xFF == ord('q'):
+# Main loop
+try:
+    while True:
+        success, imgOriginal = cap.read()
+        if not success:
+            print("Cannot get pic from camera")
             break
+
+        img = cv2.resize(imgOriginal, (32, 32))  
+        img = preprocessing(img)
+
+        imgInput = img.reshape(1, 32, 32, 1)  
+
+        predictions = model.predict(imgInput)
+        classIndex = int(np.argmax(predictions))
+        probabilityVal = np.max(predictions)
+
+        print(f"Predictions: {predictions}")  
+
+        imgDisplay = (img * 255).astype(np.uint8)
+        cv2.imshow("Process Image", imgDisplay)
+
+        cv2.putText(imgOriginal, "CLASS:", (20, 35), font, 0.75, (0, 0, 255), 2)
+        cv2.putText(imgOriginal, "PROBABILITY:", (20, 75), font, 0.75, (0, 0, 255), 2)
+
+        if probabilityVal > threshold:
+            className = getClassName(classIndex)
+            cv2.putText(imgOriginal, f"{classIndex} {className}", (120, 35), font, 0.75, (0, 0, 255), 2)
+            cv2.putText(imgOriginal, f"{round(probabilityVal*100,2)}%", (180, 75), font, 0.75, (0, 0, 255), 2)
+
+        cv2.imshow("Result", imgOriginal)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            print("Stop program")
+            break
+except KeyboardInterrupt:
+    print("Stop program")
+finally:
+    cap.release()
+    cv2.destroyAllWindows()
